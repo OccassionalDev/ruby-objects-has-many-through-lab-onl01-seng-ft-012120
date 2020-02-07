3d521dc4749d7f2c2499a5e95df08ab9fdca236e
Erica Schimmel <ericaschimmel01@gmail.com>
2020-02-07 20:36:19 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,5 +1,16 @@
 class Doctor 
-  attr
+  attr_accessor :name 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    save
+  end 
+  
+  def save 
+    @@all << self 
+  end 
   
   
 end 
